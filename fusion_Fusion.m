a53759fe7e2ede30f42ff4367c2d1d6aa94c18fa
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-10-16 19:57:31 -0400

    Bug Fixing
    The program can now correctly reflect the number of bands in the ETM image.

--- fusion_Fusion.m
+++ fusion_Fusion.m
@@ -84,7 +84,7 @@ function fusion_Fusion(main)
         end
 
         ETM = multibandread([main.input.etm,File.ETM.name(1:(length(File.ETM.name)-4))],...
-            [numel(main.etm.line),numel(main.etm.sample),numel(main.etm.band)],'int16',0,main.etm.interleave,'ieee-le');
+            [numel(main.etm.line),numel(main.etm.sample),main.etm.band],'int16',0,main.etm.interleave,'ieee-le');
         ETM(ETM>10000) = nan;
         ETM(ETM<1) = nan;
 
