4921394bfd5c72915dc58dccd9330901a2da3564
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-10-22 16:03:08 -0400

    Improvement
    The script now displays some status report.

--- bsh/fusion_Batch.sh
+++ bsh/fusion_Batch.sh
@@ -9,7 +9,10 @@
 #   4.Discard rate
 #   5.n jobs
 
+echo 'Total jobs to submit is' $5
 for i in $(seq 1 $5); do
+    echo 'Submitting job no.' $i 'out of' $5
+    chmod u+x ./fusion_qsub.sh
     qsub ./fusion_qsub.sh $1 $2 $3 $4 $i $5
 done
 
