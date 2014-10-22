1fe2e7b2d8a191aaadbb1ca85f77df4c84f8406a
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-10-22 15:28:39 -0400

    Improvement
    Added input arguments.

--- bsh/fusion_Submit.sh
+++ bsh/fusion_Submit.sh
@@ -2,10 +2,8 @@
 
 # Launch Matlab in Bash and run Fusion.
 
-# cd $1
-
 matlab -nodisplay -nojvm -singleCompThread << MATLAB_ENV
-main = fusion_Inputs()
+main = fusion_Inputs($1,$2,$3,$4,[$5 $6])
 fusion_Fusion(main)
 quit
 MATLAB_ENV
