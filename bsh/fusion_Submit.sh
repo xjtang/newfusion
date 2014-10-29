8cf27ecb0cb54d438043f301fbe416f33acab3ee
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-10-29 14:49:59 -0400

    Improvement
    Including all steps.

--- bsh/fusion_Submit.sh
+++ bsh/fusion_Submit.sh
@@ -5,7 +5,10 @@
 matlab -nodisplay -nojvm -singleCompThread << MATLAB_ENV
 cd ../
 main = fusion_Inputs('$1',$2,$3,$4,[$5 $6])
+fusion_BRDF(main)
+fusion_SwathSub(main)
 fusion_Fusion(main)
+fusion_WriteHDF(main)
 quit
 MATLAB_ENV
 
