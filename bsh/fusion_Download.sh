acf374d2cf713359a8d0089534e571cdf0dd8891
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-09 22:43:02 -0500

    Update fusion_Download.sh

--- bsh/fusion_Download.sh
+++ bsh/fusion_Download.sh
@@ -34,7 +34,7 @@ echo "=========================================================="
 # Run the bash script
 module load R
 R --slave --vanilla --quiet --no-save  <<EEE
-install.packages("RCurl")
+install.packages("RCurl",repos="http://cran.rstudio.com/")
 source('https://raw.githubusercontent.com/xjtang/fusion/master/tool/swath_download.R')
 swathDownload($1,$2)
 EEE
