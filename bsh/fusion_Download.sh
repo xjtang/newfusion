a1d048477c8df1a4409ec166e3e92f63a62dc5fa
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-09 22:28:31 -0500

    Create fusion_Download.sh

---
+++ bsh/fusion_Download.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Submit qsub job 
+
+# Specify which shell to use
+#$ -S /bin/bash
+
+# Run for 24 hours
+#$ -l h_rt=24:00:00
+
+# Forward my current environment
+#$ -V
+
+# Give this job a name
+#$ -N fusion_download
+
+# Join standard output and error to a single file
+#$ -j y
+
+# Name the file where to redirect standard output and error
+#$ -o download.qlog
+
+# Now let's keep track of some information just in case anything goes wrong
+
+echo "=========================================================="
+echo "Starting on : $(date)"
+echo "Running on node : $(hostname)"
+echo "Current directory : $(pwd)"
+echo "Current job ID : $JOB_ID"
+echo "Current job name : $JOB_NAME"
+echo "Task index number : $SGE_TASK_ID"
+echo "=========================================================="
+
+# Run the bash script
+module load R
+R --slave --vanilla --quiet --no-save  <<EEE
+install.packages("RCurl")
+source('https://raw.githubusercontent.com/xjtang/fusion/master/tool/swath_download.R')
+swathDownload($1,$2)
+EEE
+
+echo "=========================================================="
+echo "Finished on : $(date)"
+echo "=="
