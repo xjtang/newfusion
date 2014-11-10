79c3ea8dd6192488a9012d8f9b03a49e101187ef
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-10 14:54:21 -0500

    Bug Fixing

--- fusion_BRDFusion.m
+++ fusion_BRDFusion.m
@@ -88,7 +88,7 @@ function fusion_BRDFusion(main)
 
         % read brdf coefficients
         ETMBRDF = multibandread([main.output.etmBRDF,File.ETMBRDF.name(1:(length(File.ETMBRDF.name)-4))],...
-            [numel(main.etm.line),numel(mainetm.sample),main.etm.band],'int16',0,'bsq','ieee-le');
+            [numel(main.etm.line),numel(main.etm.sample),main.etm.band],'int16',0,'bsq','ieee-le');
         ETMBRDF(ETMBRDF<=0) = nan;
         ETMBRDF = ETMBRDF/1000;
 
