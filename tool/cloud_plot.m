84e65001e65ba2b789d99ba22b499a5859e93d0c
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-24 13:36:04 -0500

    Update cloud_plot.m

--- tool/cloud_plot.m
+++ tool/cloud_plot.m
@@ -1,5 +1,5 @@
 % cloud_plot.m
-% Version 1.0.0
+% Version 1.0
 %
 % Project: Fusion
 % By Xiaojing Tang
@@ -19,5 +19,28 @@
 % Version 1.0 - 11/24/2014
 %   This script generates plot and table for cloud statistics of the MOD09SUB data.
 %   
+% Created on Github on 11/24/2014, check Github Commits for updates afterwards.
 %----------------------------------------------------------------
 
+function cloud_plot(path,res,outFile)
+
+  % get list of all valid files in the input directory
+  fileList = dir([path,'MOD09SUB*',num2str(res),'*.mat']);
+
+  % check if list is empty
+  if numel(fileList)<1
+    disp(['Cannot find any .mat file at ',num2str(res),'m resolution.']);
+    return;
+  end
+
+  % loop through all files in the list
+  for i = 1:numel(fileList)
+  
+  
+  
+  
+  end
+
+  % done
+
+end
