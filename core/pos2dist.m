0a2636901c9f5f2cecba861fcc8975d9d054333f
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-24 13:14:34 -0500

    Update pos2dist.m
    updated comments

--- core/pos2dist.m
+++ core/pos2dist.m
@@ -30,6 +30,7 @@
 %   2.Changed coding style.
 %   3.Modified for work flow of fusion version 6.1.
 %
+% Released on Github on 11/15/2014, check Github Commits for updates afterwards.
 %----------------------------------------------------------------
 %
 function [Distance, Bearing] = pos2dist(Lat1, Lon1, Lat2, Lon2)
