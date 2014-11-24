819533427d5a86f8fb94ded2ac8b6be4dc283dba
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-24 11:27:41 -0500

    Create cloud_plot.m

---
+++ tool/cloud_plot.m
@@ -0,0 +1,23 @@
+% cloud_plot.m
+% Version 1.0.0
+%
+% Project: Fusion
+% By Xiaojing Tang
+% Created On: 11/24/2014
+%
+% Input Arguments: 
+%   path - path to MOD09SUB m-files.
+%   res - resolusion of MODIS swath.
+%   outFile - output file.
+%   
+% Output Arguments: NA
+%
+% Usage: 
+%   1.Generate MOD09SUB m-files with the main fusion codes.
+%   2.Run this script with correct input arguments.
+%
+% Version 1.0 - 11/24/2014
+%   This script generates plot and table for cloud statistics of the MOD09SUB data.
+%   
+%----------------------------------------------------------------
+
