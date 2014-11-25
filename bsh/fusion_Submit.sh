40009ae066de2032aa17ddfcaf9645e79ec51b7e
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-24 22:34:12 -0500

    Update fusion_Submit.sh
    added extra parameter for platform

--- bsh/fusion_Submit.sh
+++ bsh/fusion_Submit.sh
@@ -4,7 +4,7 @@
 
 matlab -nodisplay -nojvm -singleCompThread << MATLAB_ENV
 cd ../
-main = fusion_Inputs('$1',$2,$3,$4,[$5 $6])
+main = fusion_Inputs('$1','$2',$3,$4,$5,[$6 $7])
 fusion_BRDF(main)
 fusion_SwathSub(main)
 fusion_Fusion(main)
