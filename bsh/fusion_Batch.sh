bcfa5d1c84c81dbca2b2ee5d7cf41577ad09a1ea
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-24 22:35:05 -0500

    Update fusion_Batch.sh
    added extra parameter for platform

--- bsh/fusion_Batch.sh
+++ bsh/fusion_Batch.sh
@@ -9,11 +9,11 @@
 #   4.Discard rate
 #   5.n jobs
 
-echo 'Total jobs to submit is' $5
-for i in $(seq 1 $5); do
-    echo 'Submitting job no.' $i 'out of' $5
+echo 'Total jobs to submit is' $6
+for i in $(seq 1 $6); do
+    echo 'Submitting job no.' $i 'out of' $6
     chmod u+x ./fusion_qsub.sh
-    qsub ./fusion_qsub.sh $1 $2 $3 $4 $i $5
+    qsub ./fusion_qsub.sh $1 $2 $3 $4 $5 $i $6
 done
 
 # end
