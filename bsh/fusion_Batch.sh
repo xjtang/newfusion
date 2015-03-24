7804bac92a445e6c5605d54d97b7dacd504152b8
Xiaojing Tang <xjtang@users.noreply.github.com>
2015-03-24 14:47:57 -0400

    Upgrade
    Updated the name of the job with job sequence

--- bsh/fusion_Batch.sh
+++ bsh/fusion_Batch.sh
@@ -15,7 +15,7 @@ echo 'Total jobs to submit is' $7
 for i in $(seq 1 $7); do
     echo 'Submitting job no.' $i 'out of' $7
     chmod u+x ./fusion_qsub.sh
-    qsub ./fusion_qsub.sh $1 $2 $3 $4 $5 $6 $i $7
+    qsub -N fusion_$i ./fusion_qsub.sh $1 $2 $3 $4 $5 $6 $i $7
 done
 
 # end
