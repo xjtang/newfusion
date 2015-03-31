b465f4ab3a992614a83b3878ee28a6f092f8b3f8
Xiaojing Tang <xjtang@bu.edu>
2015-03-31 15:46:41 -0400

    create new file

---
+++ core/change.m
@@ -0,0 +1,30 @@
+% change.m
+% Version 1.0
+% Core
+%
+% Project: Fusion
+% By: Xiaojing Tang
+% Created On: 3/31/2015
+% Last Update: 3/31/2015
+%
+% Input Arguments:
+%   TS (Matrix) - fusion time series of a pixel.
+% 
+% Output Arguments: 
+%   CHG (Matrix) - time series of change.
+%
+% Instruction: 
+%   1.Call by other scripts with correct input and output arguments.
+%
+% Version 1.0 - 3/31/2015
+%   The script fits a time series model to fusion result of a pixel
+%
+% Released on Github on 3/31/2015, check Github Commits for updates afterwards.
+%----------------------------------------------------------------
+
+function CHG = change(TS)
+
+
+
+    
+end
