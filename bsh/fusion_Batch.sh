42cb625c5b6541da76a7d26bfb87226248eaf64d
Xiaojing Tang <xjtang@users.noreply.github.com>
2015-04-03 15:09:43 -0400

    removed 2 input parameter

--- bsh/fusion_Batch.sh
+++ bsh/fusion_Batch.sh
@@ -11,11 +11,11 @@
 #   6.Discard rate
 #   7.n jobs
 
-echo 'Total jobs to submit is' $7
-for i in $(seq 1 $7); do
-    echo 'Submitting job no.' $i 'out of' $7
+echo 'Total jobs to submit is' $5
+for i in $(seq 1 $5); do
+    echo 'Submitting job no.' $i 'out of' $5
     chmod u+x ./fusion_qsub.sh
-    qsub -N fusion_$i ./fusion_qsub.sh $1 $2 $3 $4 $5 $6 $i $7
+    qsub -N fusion_$i ./fusion_qsub.sh $1 $2 $3 $4 $i $5
 done
 
 # end
