da95e71311334b1dc7f7e162edac590f0d78c532
Xiaojing Tang <xjtang@bu.edu>
2015-06-17 16:11:49 -0400

    Revert "Working Progress"
    This reverts commit 41410d9607ca2c3d31825bc1a7a04df6f9e64440.

--- core/change.m
+++ core/change.m
@@ -5,7 +5,7 @@
 % Project: Fusion
 % By: Xiaojing Tang
 % Created On: 3/31/2015
-% Last Update: 6/10/2015
+% Last Update: 6/5/2015
 %
 % Input Arguments:
 %   TS (Matrix) - fusion time series of a pixel.
@@ -16,7 +16,7 @@
 % Instruction: 
 %   1.Call by other scripts with correct input and output arguments.
 %
-% Version 1.0 - 6/10/2015
+% Version 1.0 - 6/5/2015
 %   The script fits a time series model to fusion result of a pixel
 %
 % Released on Github on 3/31/2015, check Github Commits for updates afterwards.
@@ -24,60 +24,31 @@
 
 function CHG = change(TS)
     
-    % costomized settings
-    set.minNoB = 10;
-    set.initNoB = 5;
-    set.nSD = 1.5;
-    set.csec = 5;
-    set.nSus = 3;
-
     % analyse input TS 
-    [~,nob] = size(TS);
-    
-    % initilize result
-    CHG = zeros(2,nob);
-    ETS = 1:nob;
+    [nob,nba] = size(TS);
+    nba = nba - 1;
 
-    % complie eligible observations
+    % filter in-eligible observations
     for i = nob:-1:1
         if max(TS(:,i)==-9999)
-            CHG(:,i) = -1;
-            ETS(i) = [];
+            TS(:,i) = [];
         end
     end
     
     % check total number of eligible observation
-    [~,neb] = size(ETS);
-    if neb < set.minNoB
+    [neb,~] = size(TS);
+    if neb < 10
         CHG = -1;
         return 
     end
         
-    % initilization
-    mainVec = TS(:,ETS(1:set.initNoB));
-    initMean = mean(mainVec,2);
-    initStd = std(mainVec,0,2);
-    nSusp = 0;
-    nCsec = 0;
+    % initilize result
+    CHG = zeros(2,neb);
     
     % start sequnce
-    for i = (set.initNoB+1):neb
+    for i = 1:neb
         
-        % calculate metrics
-        x = TS(:,i);
-        xRes = abs(x-initMean);
-        xNorm = xRes./initStd;
-        xDev = mean(xNorm);
-        
-        % check if possible change occured
-        if xDev >= set.nSD 
-            nSusp = nSusp + 1;
-            nCsec = nCsec + 1;
-            
-        else
-            
-            
-        end
+    
     end
 
     % finalize result
