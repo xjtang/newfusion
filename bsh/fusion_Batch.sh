f6f5b4508713e54cfac40cdb1fec8fc5c941fc8f
Xiaojing Tang <xjtang@bu.edu>
2015-07-03 21:54:04 -0400

    changes for new version

--- bsh/fusion_Batch.sh
+++ bsh/fusion_Batch.sh
@@ -4,18 +4,14 @@
 # n defines number of jobs in total
 # Input Arguments: 
 #   1.Function to execute
-#   2.Data path
-#   3.Platform
-#   4.BRDF switch
-#   5.Resolusion
-#   6.Discard rate
-#   7.n jobs
+#   2.Config File
+#   3.n jobs
 
-echo 'Total jobs to submit is' $5
-for i in $(seq 1 $5); do
-    echo 'Submitting job no.' $i 'out of' $5
+echo 'Total jobs to submit is' $3
+for i in $(seq 1 $3); do
+    echo 'Submitting job no.' $i 'out of' $3
     chmod u+x ./fusion_qsub.sh
-    qsub -N fusion_$i ./fusion_qsub.sh $1 $2 $3 $4 $i $5
+    qsub -N fusion_$i ./fusion_qsub.sh $1 $2 $i $3
 done
 
 # end
