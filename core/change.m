d3ac7803f306d581687c83dc0abc76bfe5b5b5c5
Xiaojing Tang <xjtang@users.noreply.github.com>
2015-07-12 15:17:20 -0400

    major update

--- core/change.m
+++ core/change.m
@@ -5,7 +5,7 @@
 % Project: New fusion
 % By xjtang
 % Created On: 3/31/2015
-% Last Update: 7/7/2015
+% Last Update: 7/12/2015
 %
 % Input Arguments:
 %   TS (Matrix) - fusion time series of a pixel.
@@ -30,8 +30,9 @@
 %   2.Tested.
 %   3.Fixed a dimension bug.
 %
-% Updates of Version 2.0 - 7/7/2015
+% Updates of Version 2.0 - 7/12/2015
 %   1.Completely redesigned the algorithm.
+%   2.Fixed a major bug.
 %
 % Released on Github on 3/31/2015, check Github Commits for updates afterwards.
 %----------------------------------------------------------------
@@ -90,7 +91,7 @@ function CHG = change(TS,sets)
     if sets.outlr > 0
         for i = 1:sets.outlr
             % remove outliers in the initial observations
-            subMean = (sets.weight*mainVec)/nband;
+            subMean = (sets.weight*mainVec);
             [~,TSmaxI] = max(subMean);
             [~,TSminI] = min(subMean);
             mainVec(:,[TSmaxI,TSminI]) = [];
@@ -99,19 +100,9 @@ function CHG = change(TS,sets)
     initMean = mean(mainVec,2);
     initStd = std(mainVec,0,2);
     CHGFlag = 0;
-    nonFst = 0;
-      
-    % check if this is a stable non-forest pixel
-    pMean = sets.weight*abs(initMean);
-    pSTD = sets.weight*abs(initStd);
-    if pMean > sets.nonfstmean && pSTD > sets.nonfstdev 
-        nonFst = 1;
-    end
     
     % start sequnce
     for i = 1:length(ETS)
-        
-        % start count
         
         % calculate metrics
         x = TS(:,ETS(i));
@@ -119,54 +110,62 @@ function CHG = change(TS,sets)
         xNorm = xRes./initStd;
         xDev = sets.weight*xNorm;
         
-        % check if this is a statble non-forest pixel
-        if nonFst == 0 
-            % check if possible change occured
-            if xDev >= sets.nSD 
-                % check if change already detected
-                if CHGFlag == 1
-                    % set result to changed
-                    CHG(ETS(i)) = 4;
-                else
-                    % see if this is a break
-                    if i < length(ETS)+1-sets.nCosc
-                        nSusp = 1;
-                        for k = (i+1):(i+sets.nCosc-1)
-                            xk = TS(:,ETS(k));
-                            xkRes = abs(xk-initMean);
-                            xkNorm = xkRes./initStd;
-                            xkDev = sets.weight*xkNorm;
-                            if xkDev >= sets.nSD
-                                nSusp = nSusp + 1;
-                            end
-                        end
-                        if nSusp >= sets.nSusp
-                            CHG(ETS(i)) = 3;
-                            CHGFlag = 1;
-                            continue;
+        % check if possible change occured
+        if xDev >= sets.nSD 
+            % check if change already detected
+            if CHGFlag == 1
+                % set result to changed
+                CHG(ETS(i)) = 4;
+            else
+                % see if this is a break
+                if i < length(ETS)+1-sets.nCosc
+                    nSusp = 1;
+                    for k = (i+1):(i+sets.nCosc-1)
+                        xk = TS(:,ETS(k));
+                        xkRes = abs(xk-initMean);
+                        xkNorm = xkRes./initStd;
+                        xkDev = sets.weight*xkNorm;
+                        if xkDev >= sets.nSD
+                            nSusp = nSusp + 1;
                         end
                     end
+                    if nSusp >= sets.nSusp
+                        CHG(ETS(i)) = 3;
+                        CHGFlag = 1;
+                    else
+                        CHG(ETS(i)) = 2;
+                    end
+                else
                     % this is an outlier
                     CHG(ETS(i)) = 2;
                 end
-            else
-                % check if change already detected
-                if CHGFlag == 1
-                    % set result to edge of change
-                    CHG(ETS(i)) = 5;
-                else
-                    % set result to stable
-                    CHG(ETS(i)) = 1;
-                    % update main vector
-                    if i > sets.initNoB
-                        mainVec = [mainVec,TS(:,ETS(i))];  %#ok<*AGROW>
-                        initMean = mean(mainVec,2);
-                        initStd = std(mainVec,0,2);
-                    end
-                end
             end
         else
-            % deal with stable non-forest pixel
+            % check if change already detected
+            if CHGFlag == 1
+                % set result to edge of change
+                CHG(ETS(i)) = 5;
+            else
+                % set result to stable
+                CHG(ETS(i)) = 1;
+                % update main vector
+                if i > sets.initNoB
+                    mainVec = [mainVec,TS(:,ETS(i))];  %#ok<*AGROW>
+                    initMean = mean(mainVec,2);
+                    initStd = std(mainVec,0,2);
+                end
+            end
+        end
+
+    end
+    
+    % check if this is a statble non-forest pixel
+    pMean = sets.weight*abs(initMean);
+    pSTD = sets.weight*abs(initStd);
+    if pMean > sets.nonfstmean && pSTD > sets.nonfstdev 
+        % deal with stable non-forest pixel
+        for i = 1:length(ETS)
+            x = TS(:,ETS(i));
             if sets.weight*abs(x) >= sets.nonfstedge
                 CHG(ETS(i)) = 6;
             else
