7af99d0bcf77663ad4fb5698d257d083500500b9
Xiaojing Tang <xjtang@users.noreply.github.com>
2015-07-13 10:12:36 -0400

    added a new type of map

--- core/genMap.m
+++ core/genMap.m
@@ -1,11 +1,11 @@
 % genMap.m
-% Version 1.0
+% Version 1.1
 % Core
 %
 % Project: New fusion
 % By xjtang
 % Created On: 7/7/2015
-% Last Update: 7/8/2015
+% Last Update: 7/13/2015
 %
 % Input Arguments:
 %   X (Vector) - change time series
@@ -27,8 +27,17 @@
 %   1.Added new type of change map.
 %   2.Bugs fixed.
 %
+% Version 1.0.1 - 7/13/2015
+%   1.Added a new type of change map.
+%
 % Released on Github on 7/7/2015, check Github Commits for updates afterwards.
 %----------------------------------------------------------------
+%
+% Change Map Types
+%   1 - day of change
+%   2 - month of change
+%   3 - class map
+%   4 - change only
 
 function CLS = genMap(X,D,mapType,edgeThres)
     
@@ -41,7 +50,7 @@ function CLS = genMap(X,D,mapType,edgeThres)
         CLS = -9998;
         
     elseif mapType == 3
-        % change only map
+        % class map
         % deal with different types
             % stable forest
             if (max(X)<=2)&&(max(X)>=1)
@@ -64,6 +73,20 @@ function CLS = genMap(X,D,mapType,edgeThres)
             if sum(X==5) >= edgeThres(1)
                 CLS = 11;
             end
+    elseif mapType == 4
+        % change only map
+        % confirmed changed
+        if max(X==3) == 1
+            [~,breakPoint] = max(X==3);
+            % could be change edge
+            if sum(X==5) >= edgeThres(1)
+                CLS = 11;
+            else
+                CLS = 10;
+            end
+        else
+            CLS = 0;
+        end
     else
         % date of change map (default)
         % deal with different types of change
