a64d8076cdae4e3c524a3ec5960727c5845f4a3b
Xiaojing Tang <xjtang@bu.edu>
2015-12-03 13:29:20 -0500

    working progress

--- fusion_Fusion.m
+++ fusion_Fusion.m
@@ -137,7 +137,7 @@ function fusion_Fusion(main)
             
             % check if results for this file already exist
             output = dir([main.output.modsubf thisPlat '*' 'ALL' '*' DayStr '.' TimeStr '*']);
-            if exist(output,'file')>0 
+            if numel(output)<1
                 disp([DayStr ' result already exist, skip this file.']);
                 continue;
             end
