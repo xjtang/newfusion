8bde14460b0899a56c4cba7018fd0ce6e37c548e
Xiaojing Tang <xjtang@bu.edu>
2016-01-14 14:24:01 -0500

    bug fix

--- bsh/fusion_Batch.sh
+++ bsh/fusion_Batch.sh
@@ -11,7 +11,7 @@ echo 'Total jobs to submit is' $3
 for i in $(seq 1 $3); do
     echo 'Submitting job no.' $i 'out of' $3
     chmod u+x ./fusion_Submit.sh
-    qsub -N fusion_$i ./fusion_qsub.sh $1 $2 $i $3
+    qsub -N fusion_$i ./fusion_Submit.sh $1 $2 $i $3
 done
 
 # end
