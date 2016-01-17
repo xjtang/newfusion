be39fb953c0d029b3ca403375ad7afb90fc16779
Xiaojing Tang <xjtang@bu.edu>
2016-01-16 22:45:14 -0500

    bash script for compiling

---
+++ bsh/fusion_Compile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Compile
+
+# Specify which shell to use
+#$ -S /bin/bash
+
+# Run for 24 hours
+#$ -l h_rt=24:00:00
+
+# Forward my current environment
+#$ -V
+
+# Join standard output and error to a single file
+#$ -j y
+
+# Name the file where to redirect standard output and error
+#$ -o compile.qlog
+
+# Now let's keep track of some information just in case anything goes wrong
+
+echo "=========================================================="
+echo "Starting on : $(date)"
+echo "Running on node : $(hostname)"
+echo "Current directory : $(pwd)"
+echo "Current job ID : $JOB_ID"
+echo "Current job name : $JOB_NAME"
+echo "Task index number : $SGE_TASK_ID"
+echo "=========================================================="
+
+# Run the bash script
+matlab -nodisplay -nojvm -singleCompThread << MATLAB_ENV
+cd ../
+compile()
+quit
+MATLAB_ENV
+
+echo "=========================================================="
+echo "Finished on : $(date)"
+echo "=="
