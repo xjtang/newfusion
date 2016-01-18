a008268d6348366e6c2b3a3ba29cc32b668a73e2
Xiaojing Tang <xjtang@bu.edu>
2016-01-17 22:27:15 -0500

    bash scripts for running standalone version in batch

--- bsh/fusion_Batch.sh
+++ bsh/fusion_Batch.sh
@@ -3,15 +3,25 @@
 # submit n jobs to run fusion
 # n defines number of jobs in total
 # Input Arguments: 
+#   -c run compiled version (optional)
 #   1.Function to execute
 #   2.Config File
 #   3.n jobs
 
-echo 'Total jobs to submit is' $3
-for i in $(seq 1 $3); do
-    echo 'Submitting job no.' $i 'out of' $3
-    chmod u+x ./fusion_Submit.sh
-    qsub -N fusion_$i ./fusion_Submit.sh $1 $2 $i $3
-done
+if [ $1 = '-c' ]; then
+    echo 'Running standalone job.'
+    echo 'Total jobs to submit is' $4
+    for i in $(seq 1 $4); do
+        echo 'Submitting job no.' $i 'out of' $4
+        qsub -N fusion_$i ./fusion_Standalone.sh $3 $i $4 $2 
+    done
+else
+    echo 'Running regular job.'
+    echo 'Total jobs to submit is' $3
+    for i in $(seq 1 $3); do
+        echo 'Submitting job no.' $i 'out of' $3
+        qsub -N fusion_$i ./fusion_Submit.sh $1 $2 $i $3
+    done
+fi
 
 # end
---
+++ bsh/fusion_Standalone.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+# Submit qsub job 
+
+# Specify which shell to use
+#$ -S /bin/bash
+
+# Run for 24 hours
+#$ -l h_rt=48:00:00
+
+# Forward my current environment
+#$ -V
+
+# Join standard output and error to a single file
+#$ -j y
+
+# Name the file where to redirect standard output and error
+#$ -o fusion.qlog
+
+# request more memory
+#$ -pe omp 3
+
+# Now let's keep track of some information just in case anything goes wrong
+
+echo "=========================================================="
+echo "Starting on : $(date)"
+echo "Running on node : $(hostname)"
+echo "Current directory : $(pwd)"
+echo "Current job ID : $JOB_ID"
+echo "Current job name : $JOB_NAME"
+echo "Task index number : $SGE_TASK_ID"
+echo "=========================================================="
+
+# load module
+module load hdf/4.2.5
+module load gdal/1.10.0
+module load mcr/8.1_2013a
+# module load mcr/9.0_2015b
+
+# Run the standalone versiopn
+cd ../mcc/
+./fusion_v130 $1 $2 $3 $4
+
+echo "=========================================================="
+echo "Finished on : $(date)"
+echo "=="
