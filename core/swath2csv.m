e544846831602ca1d0dabac11f4fb647f2b94d46
xjtang <xjtang@bu.edu>
2017-07-06 22:13:32 -0400

    Calculate bearing in UTM

--- core/swath2csv.m
+++ core/swath2csv.m
@@ -5,7 +5,7 @@
 % Project: New Fusion
 % By xjtang
 % Created On: 6/28/2017
-% Last Update: 7/3/2017
+% Last Update: 7/6/2017
 %
 % Input Arguments:
 %   MOD09SUB (Structure) - Subset of MODIS swath data over the area of the ETM image and the corresponding geometry information.
@@ -17,7 +17,7 @@
 % Instruction:
 %   1.Call by other scripts with correct input and output arguments.
 %
-% Version 1.0 - 7/3/2017
+% Version 1.0 - 7/6/2017
 %   Function to convert swath image to a table of actual coordinates and axis of the observation footprints
 %
 %----------------------------------------------------------------
@@ -30,19 +30,23 @@ function [header, csv] = swath2csv(MOD09SUB, res, utm)
         SizeAlongTrack = MOD09SUB.SizeAlongTrack250;
         Lat = MOD09SUB.Lat250;
         Lon = MOD09SUB.Lon250;
-        MODBear = MOD09SUB.Bearing250;
     elseif res == 500
         SizeAlongScan = MOD09SUB.SizeAlongScan500;
         SizeAlongTrack = MOD09SUB.SizeAlongTrack500;
         Lat = MOD09SUB.Lat500;
         Lon = MOD09SUB.Lon500;
-        MODBear = MOD09SUB.Bearing500;
     else
         error('Invalid resolution');
     end
 
     % convert lat lon to utm
     [East,North,~] = deg2utm(Lat,Lon,utm);
+    
+    % calculate bearing
+    Bearing = nan(size(East));
+    [~, Bearing(:,1:end-1)] = pos2dist(East(:,1:end-1),North(:,1:end-1),...
+        East(:,2:end),North(:,2:end));
+    Bearing(:,end) = 2*Bearing(:,end-1)-Bearing(:,end-2);
     
     % initialize output
     csv = zeros(numel(Lat),10);
@@ -61,7 +65,7 @@ function [header, csv] = swath2csv(MOD09SUB, res, utm)
             csv(count,5) = Lon(Index_Row,Index_Col);
             csv(count,6) = East(Index_Row,Index_Col);
             csv(count,7) = North(Index_Row,Index_Col);
-            csv(count,8) = MODBear(Index_Row,Index_Col);
+            csv(count,8) = Bearing(Index_Row,Index_Col);
             % A and B for a oval shape
             csv(count,9) = SizeAlongScan(Index_Row,Index_Col);
             csv(count,10) = SizeAlongTrack(Index_Row,Index_Col)/2;
